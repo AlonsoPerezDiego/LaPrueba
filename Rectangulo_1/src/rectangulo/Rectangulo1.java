559d39ce91c7454bb6e371415971a00756ddb61b
dalonsoperez <dalonsoperez@atanasoff23.danielcastelao.org>
2016-10-10 09:14:19 +0200

    Cambiado nombre

---
+++ Rectangulo_1/src/rectangulo/Rectangulo1.java
@@ -0,0 +1,14 @@
+
+package rectangulo;
+
+
+public class Rectangulo1 {
+
+ 
+    public static void main(String[] args) {
+        int base = 3;
+        int altura = 4;
+                System.out.println("área rectangulo de base " + base + "cm e altura " + altura + "cm = " + base * altura + "cm");
+    }
+    
+}
