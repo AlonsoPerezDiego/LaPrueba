029be34fa4fb3fe6e28e1d773bc6f923f82a9ac0
dalonsoperez <dalonsoperez@atanasoff23.danielcastelao.org>
2016-10-10 08:52:45 +0200

    Rectangulo 2

---
+++ Rectangulo_2/src/rectangulo/Rectangulo.java
@@ -0,0 +1,14 @@
+
+package rectangulo;
+
+
+public class Rectangulo {
+
+ 
+    public static void main(String[] args) {
+        float base = 3;
+        float altura = 4.0F;
+                System.out.println("área rectangulo de base " + base + "cm e altura " + altura + "cm = " + base * altura + "cm");
+    }
+    
+}
